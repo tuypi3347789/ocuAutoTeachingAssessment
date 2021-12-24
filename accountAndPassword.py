aaacad1f001dac342ee4949fe2a5d192819cbcdc
forwork852020 <forwork852020@gmail.com>
2021-12-24 21:30:53 +0800

    revise

--- accountAndPassword.py
+++ accountAndPassword.py
@@ -1,4 +1,4 @@
 import os
 
-account = os.environ.get('ACCOUNT', '10825526')
-password = os.environ.get('PASSWORD', 'PQpq10825526')
+account = os.environ.get('ACCOUNT', '')
+password = os.environ.get('PASSWORD', '')
